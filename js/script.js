aee414d303bf2d835af87aa5a6d45504241278c0
Muhammad fachri Afif <fachryafif86@gmail.com>
2024-02-29 22:31:00 +0700

    membuat website kedai kopi kenangan senja

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Toggle
+const navbarNav = document.querySelector('.navbar-nav');
+const hamburgerMenu = document.getElementById('hamburger-menu');
+hamburgerMenu.onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+document.addEventListener('click', function (e) {
+  if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
